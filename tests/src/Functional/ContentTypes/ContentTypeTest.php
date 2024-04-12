be6d6cf2ab8f6e5539d86830f36ee74989b20c81
Lacey Sanderson <laceyannesanderson@gmail.com>
2024-04-12 15:21:39 -0600

    Add assertions to confirm the expected content types and fields are there.

--- tests/src/Functional/ContentTypes/ContentTypeTest.php
+++ tests/src/Functional/ContentTypes/ContentTypeTest.php
@@ -34,8 +34,11 @@ class ContentTypeTest extends ChadoTestBrowserBase {
    */
   protected $expected_contenttypes = [
     'Experiment Types' => [
-      'field_experiment', 'greenhouse_experiment', 'growthchamber_experiment',
-      'biochem_experiment', 'ecosystem_survey'
+      'field_experiment' => 10,
+      'greenhouse_experiment' => 10,
+      'growthchamber_experiment' => 10,
+      'biochem_experiment' => 10,
+      'ecosystem_survey' => 10,
     ],
   ];
 
@@ -64,5 +67,28 @@ class ContentTypeTest extends ChadoTestBrowserBase {
 
     // Then import the content types and their fields.
     \trpcultivate_import_contenttypes();
+
+    // Now select all content types by category and see if they match expectations.
+    foreach ($this->expected_contenttypes as $category => $expected_types) {
+      $found_types = \Drupal::entityTypeManager()
+        ->getStorage('tripal_entity_type')
+        ->loadByProperties(['category' => $category]);
+      $expected_count = sizeof($expected_types);
+      $this->assertCount($expected_count, $found_types,
+        "We did not get the expected number of types in the $category category.");
+      foreach ($expected_types as $expected_id => $expected_field_count) {
+        $this->assertArrayHasKey($expected_id, $found_types,
+          "This particular type was expected but not found when selecting by category $category.");
+
+        // Now check that this content type has fields.
+        $found_fields = \Drupal::service('entity_field.manager')
+          ->getFieldDefinitions('tripal_entity', $expected_id);
+        // This returns the 8 base fields too (i.e. id, type, uid, title, status, created, changed)
+        // so we add them to the list.
+        $expected_count = $expected_field_count + 8;
+        $this->assertCount($expected_count, $found_fields,
+          "We did not see the expected number of fields attached to the $expected_id content type.");
+      }
+    }
   }
 }
