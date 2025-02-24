4ef28f26103c68614ce02a208ec753bf2adac893
Carolyn Caron <carolyn.caron@usask.ca>
2025-02-24 16:38:13 -0600

    Created a new Organism trait for setting/getting organism IDs

---
+++ src/TripalCultivateValidator/ValidatorTraits/Organism.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Drupal\trpcultivate\TripalCultivateValidator\ValidatorTraits;
+
+use Drupal\tripal_chado\Database\ChadoConnection;
+
+/**
+ * Provides getters/setters regarding organism and genus.
+ */
+trait Organism {
+
+  /**
+   * A Database query interface for querying Chado using Tripal DBX.
+   *
+   * @var Drupal\tripal_chado\Database\ChadoConnection
+   */
+  protected ChadoConnection $chado_connection;
+
+  /**
+   * Sets a single organism from its ID.
+   *
+   * NOTE: This setter is generally for validators that need the exact organism
+   * ID of the data type being inserted.
+   *
+   * @param int $organism_id
+   *   A single organism id from the chado.organism table.
+   */
+  public function setOrganismID(int $organism_id) {
+
+    // Query the organism ID in chado.
+    $query = $this->chado_connection->select('1:organism', 'o')
+      ->fields('o', ['organism_id'])
+      ->condition('o.organism_id', $organism_id);
+    $record = $query->execute()->fetchObject();
+    if (is_object($record)) {
+      $this->context['organism_ids'] = [$organism_id];
+    }
+    else {
+      // Since this is a user-provided value, the error is going to be logged
+      // instead of thrown as an exception and then checked by a validator so
+      // that the error can be passed to the user in a friendly way.
+      // Log the ID to watchdog (the admin UI).
+      // Log to the user a generic message that the organism doesn't exist.
+    }
+
+  }
+
+  /**
+   * Sets all of the available organism IDs for a particular genus.
+   *
+   * NOTE: This setter is helpful for validators that only perform a lookup for
+   * existing data types related to an organism (such as germplasm).
+   *
+   * @param string
+   *   The genus name.
+   */
+  public function setGenus(string $genus) {
+
+    // Query the genus in chado.
+    $query = $this->chado_connection->select('1:organism', 'o')
+      ->fields('o', ['organism_ids'])
+      ->condition('o.genus', $genus);
+    $record = $query->execute()->fetchAll();
+
+  }
+
+  /**
+   * Returns the organism(s).
+   *
+   * @return array
+   *   A list of organisms that were set.
+   *
+   * @throws \Exception
+   *   - If an organism ID was not set by setOrganismID() or setGenus().
+   */
+  public function getOrganismIDs() {
+
+    if (array_key_exists('organism_ids', $this->context)) {
+      return $this->context['organism_ids'];
+    }
+    else {
+      throw new \Exception("Cannot retrieve an array of organism IDs as one has not been set by either the setOrganismID() or setGenus() method.");
+    }
+  }
+
+}
