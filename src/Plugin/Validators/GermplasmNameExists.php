718b656b0afe5e4d63dd87d4be55bb0ed163e0dc
Carolyn Caron <carolyn.caron@usask.ca>
2025-05-26 17:08:33 -0600

    Started on logic for the process method

--- src/Plugin/Validators/GermplasmNameExists.php
+++ src/Plugin/Validators/GermplasmNameExists.php
@@ -204,7 +204,9 @@ class GermplasmNameExists extends TripalCultivateValidatorBase implements Contai
   /**
    * Process failed validation from GermplasmNameExists into a render array.
    *
-   * @param array $failure
+   *
+   *
+   * @param array $failures
    *   An associative array that was returned by the GermplasmNameExists
    *   validator in the event of failed validation. It contains the following
    *   keys:
@@ -228,6 +230,16 @@ class GermplasmNameExists extends TripalCultivateValidatorBase implements Contai
    *   - 'case-message-2': the message when a project has no genus set to it.
    *   - 'case-message-3': the message when the genus selected by the user is
    *     not configured to the selected project.
+   * @param array $metadata
+   *   An array of additional metadata (or contextual information) needed for
+   *   by the process method. Here, the following keys are expected:
+   *   - 'column_headers': This contains an array of column headers that are
+   *     expected to contain germplasm names. The index in this array MUST match
+   *     the position (starting with 0) of the column in the input file. Eg:
+   *     'column_headers' => [
+   *       '2' => 'Maternal Germplasm Name', // Header of column #3
+   *       '4' => 'Paternal Germplasm Name', // Header of column #5
+   *     ].
    *
    * @return array
    *   A render array of type "item" used to display feedback to the user about
@@ -236,19 +248,118 @@ class GermplasmNameExists extends TripalCultivateValidatorBase implements Contai
    *   - The 'items' include a table for each potential case in the $failures
    *     array:
    *     - If a germplasm name is empty
-   *       - Headers include 'Row Number'
+   *       - Headers include 'Row Number', 'Column Header'
    *     - A duplicate germplasm name seen in the database
-   *       - Headers include 'Row Number', 'Germplasm Name', 'Duplicate records'
+   *       - Headers include 'Row Number', 'Column Header', 'Germplasm Name'
    *     - A missing germplasm name from the database
-   *       - Headers include 'Row Number', 'Germplasm Name'
+   *       - Headers include 'Row Number', 'Column Header', 'Germplasm Name'
    *
    * @throws \Exception
    *   - If the failure parameter was not formatted properly.
    *   - If the case string returned by the validator implied validation passed.
    *   - If the case string returned by the validator is not recognized.
    */
-  public static function processSimpleList(array $failure, array $tokens = []) {
+  public static function process(array $failures, array $tokens = [], array $metadata) {
+    // Define our table header.
+    // We will start with the line number and build the header from there as we
+    // go through the failures. There will be a column for each column checked
+    // by this validator instance and the column header will be the same as it
+    // appears in the file.
+    $table_header = [-1 => 'Line Number'];
+    // For this validator there are can be up to 2 tables:
+    // - 'table'->'missing': Germplasm name could not be found in the database.
+    // - 'table'->'duplicate': Germplasm name has duplicates in the database.
+    $table = [];
 
+    // Loop through each row in the $failures array and piece apart the
+    // different cases into different tables.
+    foreach ($failures as $line_no => $validation_result) {
+      // @todo Check the format of the validation_result parameter.
+      // $this->checkValidationStatusArray($validation_result, 'GermplasmNameExists', $line_no);
+      // Keeps track of which table this one line's validation result gets added
+      // to based on the case it triggered.
+      if ($validation_result['case'] == 'Unable to lookup germplasm with empty values') {
+        // Create a helper method.
+      }
+      $table_case = [];
+      if ($validation_result['case'] == 'Missing germplasm name(s) in the database') {
+        $table_case = ['missing_cells'];
+      }
+      elseif ($validation_result['case'] == 'Duplicate(s) found in the database for germplasm name(s)') {
+        $table_case = ['duplicate_cells'];
+      }
+      elseif ($validation_result['case'] == 'Missing germplasm name(s) and found duplicate(s) in the database') {
+        $table_case = ['missing_cells', 'duplicate_cells'];
+      }
+      elseif ($validation_result['case'] == 'Germplasm name(s) exist(s) in the database') {
+        throw new \Exception("The case string returned by the GermplasmNameExists validator at line #$line_no implies validation passed, but valid is set to FALSE.");
+      }
+      else {
+        throw new \Exception("The case string returned by the GermplasmNameExists validator at line #$line_no is not recognized as a potential case.");
+      }
+      // Now set values that should appear for this row in the table(s) for this
+      // particular case.
+      foreach ($table_case as $case) {
+        // Declare the array storing rows for this table, if not already.
+        if (!array_key_exists($case, $table)) {
+          $table[$case]['rows'] = [];
+        }
+        // Iterate through each index, and check our tokens array for the
+        // appropriate column header.
+        foreach ($validation_result['failedItems'][$case] as $index => $cell) {
+
+        }
+
+        array_push($table[$case]['rows'], [
+          $line_no,
+          $validation_result['failedItems']['combo_provided'][$trait],
+          $validation_result['failedItems']['combo_provided'][$method],
+          $validation_result['failedItems']['combo_provided'][$unit],
+        ]);
+      }
+    }
+    // Check which tables were created, and assign the correct message.
+    // Note that both tables can exist at the same time, hence not an 'elseif'.
+    if (array_key_exists('file', $table)) {
+      $table['file']['message'] = 'These trait-method-unit combinations occurred multiple times within your input file. The line number indicates the duplicated occurrence(s).';
+    }
+    if (array_key_exists('database', $table)) {
+      $table['database']['message'] = 'These trait-method-unit combinations have already been imported into this site. Please confirm the existing version fully represents your data. If it does, then you can remove it from your input file. If not then you need to make the names more specific.';
+    }
+
+    // Finally, loop through our tables and build our render array.
+    $tables = [];
+    foreach ($table as $table_key => $table_case) {
+      array_push($tables, [
+        [
+          '#prefix' => '<div class="case-message case-' . $table_key . '">',
+          '#markup' => $table_case['message'],
+          '#suffix' => '</div>',
+        ],
+        [
+          '#type' => 'table',
+          '#header' => $table_header,
+          '#attributes' => [
+            'class' => [
+              'table-case-' . $table_key,
+            ],
+          ],
+          '#rows' => $table_case['rows'],
+        ],
+      ]);
+    }
+    $render_array = [
+      '#theme' => 'item_list',
+      '#type' => 'ul',
+      '#attributes' => [
+        'class' => [
+          'tcp-germplasm-name-exists-failures',
+        ],
+      ],
+      '#items' => $tables,
+    ];
+
+    return $render_array;
   }
 
 }
