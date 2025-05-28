b5c4a03a8623f72b45e50bc49a47635d3a087538
Carolyn Caron <carolyn.caron@usask.ca>
2025-05-28 16:52:24 -0600

    Create a test class for the GermplasmNameExists processor method

---
+++ tests/src/Kernel/Validators/ValidatorGermplasmNameExistsProcessTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Drupal\Tests\trpcultivate_phenotypes\Kernel\Validators;
+
+use Drupal\Core\Render\Renderer;
+use Drupal\Tests\tripal_chado\Kernel\ChadoTestKernelBase;
+use Drupal\tripal_chado\Database\ChadoConnection;
+use Drupal\trpcultivate\TripalCultivateValidator\TripalCultivateValidatorManager;
+
+/**
+ * Tests any message processing methods for the GermplasmNameExists validator.
+ *
+ * @group trpcultivate
+ * @group validators
+ */
+class ValidatorGermplasmNameExistsProcessTest extends ChadoTestKernelBase {
+
+  /**
+   * Plugin Manager service.
+   *
+   * @var \Drupal\trpcultivate\TripalCultivateValidator\TripalCultivateValidatorManager
+   */
+  protected TripalCultivateValidatorManager $plugin_manager;
+
+  /**
+   * A Database query interface for querying Chado using Tripal DBX.
+   *
+   * @var \Drupal\tripal_chado\Database\ChadoConnection
+   */
+  protected ChadoConnection $chado_connection;
+
+  /**
+   * Drupal render service.
+   *
+   * @var Drupal\Core\Render\RendererInterface
+   */
+  protected Renderer $renderer;
+
+  /**
+   * Theme used in the test environment.
+   *
+   * @var string
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'system',
+    'user',
+    'file',
+    'tripal',
+    'tripal_chado',
+    'tripal_layout',
+    'trpcultivate',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    // Ensure we see all logging in tests.
+    \Drupal::state()->set('is_a_test_environment', TRUE);
+
+    // Test Chado database.
+    // Create a test chado instance and then set it in the container for use by
+    // our service.
+    $this->chado_connection = $this->createTestSchema(ChadoTestKernelBase::PREPARE_TEST_CHADO);
+    $this->container->set('tripal_chado.database', $this->chado_connection);
+
+    // Set plugin manager service.
+    $this->plugin_manager = \Drupal::service('plugin.manager.trpcultivate_validator');
+
+    // Get our renderer.
+    $this->renderer = $this->container->get('renderer');
+  }
+
+  /**
+   * Data Provider for processor for GermplasmNameExists()
+   *
+   * @return array
+   *   Each scenario is an array with the following:
+   *   - The validation result array that gets passed to the process method. It
+   *     contains the following keys:
+   *     - 'case': a developer-focused string describing the case checked.
+   *     - 'valid': FALSE to indicate that validation failed.
+   *     - 'failedItems': an array of items that failed with the following keys.
+   *       @todo summarize the failedItems that are used by this processor
+   *   - An array of tokens to use for altering the messages that get displayed
+   *     to the user. The key is the token, (ex. 'project'), and the value is
+   *     the new value to be shown for that token.
+   *   - An array of expectations in the rendered output which has the following
+   *     keys:
+   *     - 'expected_message': The message expected in the return value of the
+   *       process method for this scenario.
+   *     - 'expected_item_count': The number of failed items expected.
+   *     - 'expected_items': An array of the expected failed items.
+   */
+  public function provideGermplasmNameExistsFailedCases() {
+    $scenarios = [];
+
+    return $scenarios;
+
+  }
+
+}
