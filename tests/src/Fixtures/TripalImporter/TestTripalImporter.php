e9640ccd940d271f171dd76eded646f59ab4a09c
RLT <reynoldltan@gmail.com>
2025-06-06 15:25:44 -0600

    Create a test importer

---
+++ tests/src/Fixtures/TripalImporter/TestTripalImporter.php
@@ -0,0 +1,365 @@
+<?php
+
+namespace Drupal\trpcultivate\Plugin\TripalImporter;
+
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Url;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\tripal_chado\Database\ChadoConnection;
+use Drupal\tripal_chado\TripalImporter\ChadoImporterBase;
+use Drupal\trpcultivate\TripalCultivateValidator\TripalCultivateValidatorManager;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * This is a test Tripal Importer for testing APIs.
+ *
+ * NOTE: do not edit annotations.
+ *
+ * @TripalImporter(
+ *   id = "trpcultivate-test-importer",
+ *   label = @Translation("Tripal Cultivate: Test Importer"),
+ *   description = @Translation("A Tripal Importer used to test APIs."),
+ *   file_types = {"tsv"},
+ *   upload_description = @Translation("Please provide a data file."),
+ *   upload_title = @Translation("Import data file*"),
+ *   use_analysis = FALSE,
+ *   require_analysis = FALSE,
+ *   use_button = True,
+ *   submit_disabled = FALSE,
+ *   button_text = "Import",
+ *   file_upload = TRUE,
+ *   file_local = FALSE,
+ *   file_remote = FALSE,
+ *   file_required = TRUE,
+ *   cardinality = 1,
+ *   menu_path = "",
+ *   callback = "",
+ *   callback_module = "",
+ *   callback_path = "",
+ * )
+ */
+class TestTripalImporter extends ChadoImporterBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Headers required by this importer.
+   *
+   * @var array
+   *
+   * The following keys are required:
+   * - 'name': The column header name as it should appear in the input file.
+   * - 'description': A user-friendly description of the header that will be
+   *   displayed to the user through the form.
+   * - 'type': one of "required" or "optional" to indicate whether the column
+   *   needs to have values present or not.
+   *
+   * NOTE: Order MUST reflect the desired order of headers in the input file.
+   */
+  private array $headers = [
+    [
+      'name' => 'Header 1',
+      'description' => 'The header 1',
+      'type' => 'required',
+    ],
+    [
+      'name' => 'Header 2',
+      'description' => 'The header 2',
+      'type' => 'required',
+    ],
+    [
+      'name' => 'Header 3',
+      'description' => 'The header 3',
+      'type' => 'required',
+    ],
+
+    // Add more headers here.
+  ];
+
+  /**
+   * The key to reference the validation result array in Drupal storage system.
+   *
+   * @var string
+   */
+  private const VALIDATION_RESULT = 'validation_result';
+
+  /**
+   * The Drupal Messenger Service.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $service_Messenger;
+
+  /**
+   * The Entity Type Manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  protected EntityTypeManager $service_entityTypeManager;
+
+  /**
+   * The TripalCultivate validator plugin manager.
+   *
+   * @var \Drupal\trpcultivate\TripalCultivateValidator\TripalCultivateValidatorManager
+   */
+  protected TripalCultivateValidatorManager $service_validatorPluginManager;
+
+  /**
+   * Constructs the Phenotypes Share importer.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param Drupal\tripal_chado\Database\ChadoConnection $chado_connection
+   *   The connection to the Chado database.
+   * @param Drupal\trpcultivate\TripalCultivateValidator\TripalCultivateValidatorManager $service_validatorPluginManager
+   *   The TripalCultivate validator plugin manager.
+   * @param Drupal\Core\Entity\EntityTypeManager $service_entityTypeManager
+   *   The entity type manager.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The Drupal messenger service.
+   */
+  public function __construct(
+    array $configuration,
+    string $plugin_id,
+    mixed $plugin_definition,
+    ChadoConnection $chado_connection,
+    TripalCultivateValidatorManager $service_validatorPluginManager,
+    EntityTypeManager $service_entityTypeManager,
+    MessengerInterface $messenger,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $chado_connection);
+
+    // This test importer is for testing purposes only and is accessible only
+    // through a localhost.
+    if (strpos($_SERVER['HTTP_HOST'], 'localhost') !== 0) {
+      $home = Url::fromRoute('<front>')->toString();
+
+      $redirect = new RedirectResponse($home);
+      $redirect->send();
+      exit();
+    }
+
+    $this->service_validatorPluginManager = $service_validatorPluginManager;
+    $this->service_entityTypeManager = $service_entityTypeManager;
+    $this->service_Messenger = $messenger;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('tripal_chado.database'),
+      $container->get('plugin.manager.trpcultivate_validator'),
+      $container->get('entity_type.manager'),
+      $container->get('messenger'),
+    );
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * THIS METHOD IS REQUIRED!
+   */
+  public function configureValidators(array $form_values, string $file_mime_type) {
+
+    $validators = [];
+
+    // SEE src/Plugins/Validators for VALIDATOR IDs.
+    // SEE TripalCultivateValidator/ValidatorTraits for setters and getters.
+    //
+    // CONFIGURATION TEMPLATE:
+    $id = 'REPLACE WITH VALIDATOR ID ANNOTATION - @id';
+    $input_type = 'REPLACE WITH VALIDATOR INPUT TYPE - @input_types';
+    $instance = $this->service_validatorPluginManager->createInstance($id);
+    //
+    // Use relevant setters to set some values.
+    // For example:
+    // $instance->setExpectedColumns($num_columns, TRUE);
+    // $instance->setFileMimeType($file_mime_type);
+    // $instance->setHeaders($this->headers);
+    // $instance->setIndices(1);
+    //
+    // Register the validator instance.
+    $validators[$input_type][$id] = $instance;
+
+    return $validators;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * THIS METHOD IS REQUIRED!
+   */
+  public function processValidationMessages($failures) {
+
+    $messages = [];
+    // SEE src/Plugins/Validators for VALIDATOR IDs.
+    //
+    // PROCESS MESSAGE TEMPLATE:
+    // 1: Create a $messages entry.
+    $id = 'REPLACE WITH VALIDATOR ID ANNOTATION - @id';
+    $messages[$id] = [
+      'title' => 'REPLACE WITH A MESSAGE TITLE',
+      'status' => 'todo',
+      'details' => '',
+    ];
+
+    // 2: Inspect validator-specific $failures and switch status accordingly.
+    if (array_key_exists($id, $failures)) {
+      if (!empty($failures[$id])) {
+        $messages[$id]['status'] = 'fail';
+        $messages[$id]['details'] = 'USE VALIDATOR MESSAGE PROCESSOR HERE';
+        // For example using valid_delimiter_file validator message processsor:
+        // ... = ValidDelimitedFile::processValidDelimitedFileFailures($failures[$id]);.
+      }
+      else {
+        $messages[$id]['status'] = 'pass';
+      }
+    }
+
+    return $messages;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * THIS METHOD IS REQUIRED!
+   */
+  public function formValidate($form, &$form_state) {
+
+    $form_values = $form_state->getValues();
+    $file_id = $form_values['file_upload'];
+
+    // Load our file object.
+    $file = $this->service_entityTypeManager->getStorage('file')->load($file_id);
+
+    // Get the mime type which is used to validate the file and split the rows.
+    $file_mime_type = $file->getMimeType();
+
+    // Configure the validators.
+    $validators = $this->configureValidators($form_values, $file_mime_type);
+    $validator_key = array_keys($validators)[0];
+
+    // A Flag to keep track if any validator fails.
+    $failed_validator = FALSE;
+
+    // Holds failed items.
+    $failures = [];
+
+    // Data validation.
+    if ($failed_validator === FALSE) {
+      $file_uri = $file->getFileUri();
+      $handle = fopen($file_uri, 'r');
+      $line_no = 0;
+
+      while (!feof($handle)) {
+        $line = fgets($handle);
+        $line_no++;
+        if (empty(trim($line))) {
+          continue;
+        }
+
+        foreach ($validators[$validator_key] as $validator_name => $validator) {
+          if (!array_key_exists($validator_name, $failures)) {
+            $failures[$validator_name] = [];
+          }
+
+          // CALL THE VALIDATOR VALIDATE METHOD WITH THE REQUIRED PARAMETERS.
+          $result = 'CALL VALIDATOR VALIDATE METHOD';
+          // For example calling the validate method of valid_delimited_file:
+          // $result = $validator->validateRawRow($line);.
+          if (array_key_exists('valid', $result) && $result['valid'] === FALSE) {
+            $failures[$validator_name][$line_no] = $result;
+          }
+        }
+      }
+
+      fclose($handle);
+    }
+
+    $validation_feedback = $this->processValidationMessages($failures);
+    $storage = $form_state->getStorage();
+    $storage[self::VALIDATION_RESULT] = $validation_feedback;
+    $form_state->setStorage($storage);
+
+    $submit_form = TRUE;
+
+    foreach ($validation_feedback as $feedback_item) {
+      if ($feedback_item['status'] == 'todo' || $feedback_item['status'] == 'fail') {
+        $submit_form = FALSE;
+        break;
+      }
+    }
+
+    if ($submit_form === FALSE) {
+      $this->service_Messenger
+        ->addError('Your file import was not successful. Please check the Validation Result Window for errors and try again.');
+
+      $form_state->setRebuild(TRUE);
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * DO NOT MODIFY.
+   */
+  public function form($form, &$form_state) {
+
+    $form = parent::form($form, $form_state);
+    $this->service_Messenger->addWarning('THIS IMPORTER IS FOR TESTING PURPOSES ONLY.');
+
+    $storage = $form_state->getStorage();
+    if (isset($storage[self::VALIDATION_RESULT])) {
+      $validation_result = $storage[self::VALIDATION_RESULT];
+
+      $form['validation_result'] = [
+        '#type' => 'inline_template',
+        '#theme' => 'validation_result_window',
+        '#data' => [
+          'validation_result' => $validation_result,
+        ],
+        '#weight' => -100,
+      ];
+    }
+
+    $form['file']['file_upload_existing']['#access'] = FALSE;
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * DO NOT MODIFY.
+   */
+  public function formSubmit($form, &$form_state) {
+
+    // Display successful message to user if file import was without any error.
+    $this->service_Messenger
+      ->addStatus('<b>Your file import was successful and a Job Process Request has been created to securely save your data.</b>');
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * DO NOT MODIFY.
+   */
+  public function run() {}
+
+  /**
+   * {@inheritdoc}
+   *
+   * DO NOT MODIFY.
+   */
+  public function postRun() {}
+
+}
