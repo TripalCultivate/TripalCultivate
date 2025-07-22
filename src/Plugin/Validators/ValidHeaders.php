206813d0079ac6633b1abd8ed91f35456a4dccd1
RLT <reynoldltan@gmail.com>
2025-07-22 10:46:45 -0600

    Define additional token

--- src/Plugin/Validators/ValidHeaders.php
+++ src/Plugin/Validators/ValidHeaders.php
@@ -38,6 +38,7 @@ class ValidHeaders extends TripalCultivateValidatorBase {
    *   - 'case-empty-headers': the message when there is no header row.
    *   - 'case-mismatch-values': the message when headers are not expected.
    *   - 'case-mismatch-count': the message when headers count is not expected.
+   *   - 'num-expected-columns': number of expected column headers.
    *
    * @see TripalCultivate/src/TripalCultivateValidator/TripalCultivateValidatorBase::$mapping
    */
@@ -175,6 +176,7 @@ class ValidHeaders extends TripalCultivateValidatorBase {
    *   - 'case-empty-headers': the message when there is no header row.
    *   - 'case-mismatch-values': the message when headers are not expected.
    *   - 'case-mismatch-count': the message when headers count is not expected.
+   *   - 'num-expected-columns': number of expected column headers.
    *
    * @return array
    *   A render array of type unordered list which is used to display feedback
