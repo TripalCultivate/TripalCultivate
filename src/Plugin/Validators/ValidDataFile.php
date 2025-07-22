fa821f8e78046014c7000f2e26b4d18fbc8b48ba
RLT <rlt002@usask.ca>
2025-07-22 15:37:14 -0600

    Update src/Plugin/Validators/ValidDataFile.php
    Co-authored-by: Carolyn T Caron <carolyn.caron@usask.ca>

--- src/Plugin/Validators/ValidDataFile.php
+++ src/Plugin/Validators/ValidDataFile.php
@@ -289,13 +289,9 @@ class ValidDataFile extends TripalCultivateValidatorBase implements ContainerFac
    *   validator. Here is the overall structure:
    *     - 'case': a developer-focused string describing the case checked.
    *     - 'valid': FALSE to indicate that validation failed.
-   *     - 'failedItems': an array of items that failed with one or more of the
-   *       following keys:
-   *         - 'filename': The provided name of the file.
-   *         - 'fid': The fid of the provided file.
-   *         - 'mime': The mime type of the input file if it is not supported.
-   *         - 'extension': The extension of the input file if not supported.
-   *         @see validateFile()
+   *     - 'failedItems': an array of items that failed, where the key is
+   *.      a shorthand of the case and the value contains the failed item.
+   *       @see validateFile()
    * @param array $tokens
    *   [OPTIONAL] An array of values to use for token replacement.
    *   @see ValidDataFile::$mapping
