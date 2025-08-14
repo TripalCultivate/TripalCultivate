30aca610e46a17b525f3a0b7a1e8295b075cd6c6
RLT <rlt002@usask.ca>
2025-08-14 16:32:46 -0600

    Update tests/src/Kernel/Validators/ValidatorValueInListProcessTest.php
    Co-authored-by: Carolyn T Caron <carolyn.caron@usask.ca>

--- tests/src/Kernel/Validators/ValidatorValueInListProcessTest.php
+++ tests/src/Kernel/Validators/ValidatorValueInListProcessTest.php
@@ -431,9 +431,8 @@ class ValidatorValueInListProcessTest extends ChadoTestKernelBase {
    *   - The line number that triggered this failed validation status.
    *     - 'case': a developer-focused string describing the case checked.
    *     - 'valid': FALSE to indicate that validation failed.
-   *     - 'failedItems': an array of items that failed with the following keys,
-   *       where the key => value pairs map to the index => cell value(s) that
-   *       failed validation.
+   *     - 'failedItems': array of items that failed, where the key => value
+   *       pairs map to the index => cell value(s) that failed validation.
    * @param array $metadata
    *   An array of additional metadata (or contextual information) needed by
    *   the process method. Here, the following keys are expected:
