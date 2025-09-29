53e3f2fd8e46030feb67a5bd36e2439fe8e3c989
Vidusha Wilpita <vwilpita@yahoo.com>
2025-09-29 16:19:10 +0000

    updated the code to use the new ChadoOrganismFormElementController class to create the field

--- src/Plugin/Field/FieldWidget/ProjectGenusWidget.php
+++ src/Plugin/Field/FieldWidget/ProjectGenusWidget.php
@@ -7,7 +7,7 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\tripal\TripalField\Attribute\TripalFieldWidget;
 use Drupal\tripal_chado\TripalField\ChadoWidgetBase;
-use Drupal\tripal_chado\Controller\ChadoOrganismAutocompleteController;
+use Drupal\tripal_chado\Controller\ChadoOrganismFormElementController;
 
 /**
  * Plugin implementation of the 'project_genus_widget' field widget.
@@ -31,10 +31,30 @@ class ProjectGenusWidget extends ChadoWidgetBase {
     $field_definition = $items[$delta]->getFieldDefinition();
     $field_name = $field_definition->get('field_name');
 
+    $options = [];
+    // Set some defaults to keep each of the fields simpler.
+    $options['select_limit'] = $this->getSelectLimit($options['select_limit'] ?? NULL);
+    $options['match_operator'] ??= $this->getSetting('match_operator') ?? 'CONTAINS';
+    $options['match_limit'] ??= $this->getSetting('match_limit') ?? 10;
+    $options['size'] ??= $this->getSetting('size');
+    $options['placeholder'] ??= $this->getSetting('placeholder');
+
+    // Get the select options for the organism select list.
+    $select_options = ChadoOrganismFormElementController::getSelectOptions($options);
+
     // Current Item Values:
     $item_vals = $items[$delta]->getValue();
     $record_id = $item_vals['record_id'] ?? 0;
-    $organism_id = $item_vals['organism_id'] ?? 0;
+
+    // Determine the current organism_id based on the scientific name value.
+    $organism_id = 0;
+    if (array_key_exists('sciname_value', $item_vals) and $item_vals['sciname_value'] != '') {
+      foreach ($select_options as $id => $sciname) {
+        if ($sciname == $item_vals['sciname_value'] . ' ') {
+          $organism_id = $id ?? 0;
+        }
+      }
+    }
 
     // ID space manager to get the terms later.
     $idSpace_manager = \Drupal::service('tripal.collection_plugin_manager.idspace');
@@ -109,10 +129,9 @@ class ProjectGenusWidget extends ChadoWidgetBase {
       '#default_value' => $sciname_value,
     ];
 
-    // Insert the select element, either a select or an autocomplete depending
-    // on the number of options.
-    $options = [];
-    $elements['organism_id'] = $element + $this->organismSelectElement($organism_id, $options);
+    // Get the orgaism select element or auto-complete element.
+    $select_element = ChadoOrganismFormElementController::getFormElement($elements, $organism_id, $options);
+    $elements['organism_id'] = $element + $select_element;
 
     // Save some initial values to allow later handling of the "Remove" button.
     // Note: We do this manually instead of using saveInitialValues() because
@@ -132,102 +151,6 @@ class ProjectGenusWidget extends ChadoWidgetBase {
   }
 
   /**
-   * Select form element generator.
-   *
-   * Note: For a small number of values this creates a select, for many values
-   * this creates an autocomplete.
-   *
-   * @param int|null $default_id
-   *   The pkey_id value of the default, if one exists.
-   * @param array $options
-   *   'match_operator' - Either "CONTAINS" or "STARTS_WITH"
-   *   'match_limit' -Number of records that the autoselect will present
-   *   'size' - Size of the autocomplete form field
-   *   'placeholder' - Placeholder before autocomplete is filled
-   *   'select_limit' - The maximum number of records for a select. If more,
-   *       then use autocomplete. Use zero if autocomplete always wanted.
-   *       If NULL or empty string, then the global setting will be used.
-   *
-   * @return array
-   *   The appropriate form element
-   */
-  protected function organismSelectElement(?int $default_id, array $options): array {
-
-    // Set some defaults to keep each of the fields simpler.
-    $options['select_limit'] = $this->getSelectLimit($options['select_limit'] ?? NULL);
-    $options['match_operator'] ??= $this->getSetting('match_operator') ?? 'CONTAINS';
-    $options['match_limit'] ??= $this->getSetting('match_limit') ?? 10;
-    $options['size'] ??= $this->getSetting('size');
-    $options['placeholder'] ??= $this->getSetting('placeholder');
-
-    $element = [];
-
-    // Construct a query
-    // A single wildcard indicates that all records are to be returned.
-    $string = '%';
-    // Add one to select limit so we know if it is exceeded.
-    $count_options = $options;
-    $count_options['match_limit'] = $options['select_limit'] + 1;
-    $query = ChadoOrganismAutocompleteController::getQuery($string, $count_options);
-
-    // Get a count of the number of possible values,
-    // unless forcing always autocomplete.
-    $count = 1;
-    if ($options['select_limit'] > 0) {
-      $count = $query->countQuery()->execute()->fetchField();
-    }
-
-    // For a large number of options, or if limit is zero, use an autocomplete.
-    if ($count > $options['select_limit']) {
-      // Look up the default value if one was specified.
-      $default_value = '';
-      if ($default_id) {
-        // We can reuse the existing query since only one change is needed.
-        $query->condition('organism_id', $default_id, '=');
-        $result = $query->execute()->fetchObject();
-        if ($result) {
-          // Strip HTML tags if present, e.g. in Pub title.
-          $default_value = strip_tags($result->organism ?? '');
-          // Append the chado pkey id value.
-          $default_value .= ' (' . $default_id . ')';
-        }
-      }
-      $element = [
-        '#type' => 'textfield',
-        '#default_value' => $default_value,
-        '#autocomplete_route_name' => 'tripal_chado.organism_autocomplete',
-        '#autocomplete_route_parameters' => ['match_limit' => $options['match_limit']],
-        '#size' => $options['size'],
-        '#placeholder' => $options['placeholder'],
-      ];
-      unset($options['size']);
-      unset($options['placeholder']);
-      $element['#autocomplete_route_parameters'] = $options;
-    }
-
-    // For a small number of options, use a select.
-    else {
-      $select_query = ChadoOrganismAutocompleteController::getQuery($string, $options);
-      $results = $select_query->execute();
-      $select_options = [];
-      while ($record = $results->fetchObject()) {
-        // Strip HTML tags if present, but this is not likely for organism.
-        $organism = strip_tags($record->abbreviation ?: $record->organism ?? '');
-        $select_options[$record->pkey] = $organism;
-      }
-      natcasesort($select_options);
-      $element = [
-        '#type' => 'select',
-        '#options' => $select_options,
-        '#default_value' => $default_id,
-        '#empty_option' => $this->t('- Select -'),
-      ];
-    }
-    $element['#element_validate'] = [[static::class, 'validateAutocomplete']];
-    return $element;
-  }
-
-  /**
    * {@inheritDoc}
    */
   public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
@@ -243,19 +166,17 @@ class ProjectGenusWidget extends ChadoWidgetBase {
     // but we can follow the same logic but looking at the organism_id
     // property and the genus_prop_id property.
     // @todo implement handling of remove empty values after select.
-
-
     // The field name for the field. There are usually multiple
     // copies of a property field, so this distinguishes them.
     $first_delta = array_key_first($values);
     $field_name = $values[$first_delta]['field_name'];
 
-    // Look up the rank term
+    // Look up the rank term.
     $storage = \Drupal::entityTypeManager()->getStorage('chado_term_mapping');
     $mapping = $storage->load('core_mapping');
     $rank_term = $this->sanitizeKey($mapping->getColumnTermId('projectprop', 'rank'));
     // Convert the widget fields into an updated $values array
-    // with the items expected by the field type
+    // with the items expected by the field type.
     $this->preMassageFormValues($values);
 
     $val = 'organism_id';
@@ -263,7 +184,7 @@ class ProjectGenusWidget extends ChadoWidgetBase {
 
     // Handle any empty values so that chado storage properly
     // deletes the linking record in chado. This happens when an
-    // existing record is changed to "- Select -"
+    // existing record is changed to "- Select -".
     $retained_records = [];
     foreach ($values as $val_key => $value) {
       if ($value[$linker_key]) {
@@ -294,7 +215,8 @@ class ProjectGenusWidget extends ChadoWidgetBase {
     $storage_values = $form_state->getStorage();
     $initial_values = $storage_values['initial_values'][$field_name];
     foreach ($initial_values as $initial_value) {
-      // For initial values, the key is always 'linker_id', regardless of $linker_key value.
+      // For initial values, the key is always 'linker_id', regardless of
+      // $linker_key value.
       $organism_id = $initial_value['organism_id'];
       if ($organism_id and !in_array($organism_id, $retained_records)) {
         // This item was removed from the form. Add back a value
@@ -305,7 +227,7 @@ class ProjectGenusWidget extends ChadoWidgetBase {
       }
     }
 
-    // Reset the weights
+    // Reset the weights.
     $i = 0;
     foreach ($values as $val_key => $value) {
       if ($values[$val_key][$val]) {
@@ -320,12 +242,12 @@ class ProjectGenusWidget extends ChadoWidgetBase {
   }
 
   /**
-   * Convert the values from the widget form fields into an updated
-   * array containing the items that are expected by the field type.
+   * Convert the values from the widget form fields into an updated array.
+   *
+   * The array should contain the items that are expected by the field type.
    *
    * @param array &$values
-   *   The values array passed to massageFormValues
-   * @return void
+   *   The values array passed to massageFormValues.
    */
   protected function preMassageFormValues(array &$values): void {
     $chado = \Drupal::service('tripal_chado.database');
@@ -336,7 +258,7 @@ class ProjectGenusWidget extends ChadoWidgetBase {
       $new_value['sciname_value'] = '';
 
       if ($value['organism_id']) {
-        if ($value['organism_id'] !=  '') {
+        if ($value['organism_id'] != '') {
           $query = $chado->select('1:organism', 'o')
             ->fields('o', ['genus', 'species'])
             ->condition('o.organism_id', $value['organism_id'], '=')
