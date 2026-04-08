7e78f843a3b7cde930196a7ddf53e6bb9a6c3781
Vidusha Wilpita <vwilpita@yahoo.com>
2026-04-08 22:53:33 +0000

    added some test cases to test the new inputtype trait

---
+++ tests/src/Kernel/Validators/Traits/ValidatorTraitInputTypeTest.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Drupal\Tests\trpcultivate\Kernel\Validators;
+
+use Drupal\Tests\tripal_chado\Kernel\ChadoTestKernelBase;
+use Drupal\Tests\trpcultivate\Kernel\Validators\FakeValidators\ValidatorInputType;
+use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
+use PHPUnit\Framework\Attributes\Group;
+
+/**
+ * Tests that the Input Types trait properly sets and retrieves the input types.
+ *
+ * @group trpcultivate
+ * @group validator_traits
+ */
+#[Group('trpcultivate')]
+#[Group('validator_traits')]
+#[RunTestsInSeparateProcesses]
+class ValidatorTraitInputTypeTest extends ChadoTestKernelBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'file',
+    'system',
+    'user',
+    'path',
+    'path_alias',
+    'views',
+    'field',
+    'field_ui',
+    'field_group',
+    'tripal',
+    'tripal_chado',
+    'tripal_layout',
+    'trpcultivate',
+  ];
+
+  /**
+   * An instance of the ValidatorInputType fake validator for testing.
+   *
+   * @var \Drupal\Tests\trpcultivate\Kernel\Validators\FakeValidators\ValidatorInputType
+   */
+  protected ValidatorInputType $instance;
+
+  /**
+   * An array of indices which are invalid.
+   *
+   * @var array
+   */
+  protected array $invalid_indices;
+
+  /**
+   * An array of indices which are valid.
+   *
+   * @var array
+   */
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    // Set test environment.
+    \Drupal::state()->set('is_a_test_environment', TRUE);
+
+    // Install module configuration.
+    $this->installConfig(['trpcultivate']);
+
+    // Setup our invalid indices array.
+    $invalid_input_types = [
+      'invalid value',
+      123,
+    ];
+    $this->invalid_input_types = $invalid_input_types;
+
+    // Setup our valid indices array.
+    $valid_input_types = [
+      'data-row',
+      'metadata',
+    ];
+    $this->valid_input_types = $valid_input_types;
+
+    // Create a fake plugin instance for testing.
+    $configuration = [];
+    $validator_id = 'validator_requiring_input_type';
+    $plugin_definition = [
+      'id' => $validator_id,
+      'validator_name' => 'Validator Using Input Type Trait',
+      'input_types' => ['metadata', 'data-row'],
+    ];
+    $instance = new ValidatorInputType(
+      $configuration,
+      $validator_id,
+      $plugin_definition
+    );
+    $this->assertIsObject(
+      $instance,
+      "Unable to create $validator_id validator instance to test the InputType trait."
+    );
+
+    $this->instance = $instance;
+  }
+
+  /**
+   * Tests InputTypeTrait::setInputType() and InputTypeTrait::getInputType().
+   */
+  public function testInputTypeSetterGetter() {
+    // Try to get indices before any have been set.
+    // Exception message should trigger.
+    $expected_message = 'Input type has not yet been set for this instance of the validator.';
+
+    $exception_caught = FALSE;
+    $exception_message = 'NONE';
+    try {
+      $this->instance->getInputType();
+    }
+    catch (\Exception $e) {
+      $exception_caught = TRUE;
+      $exception_message = $e->getMessage();
+    }
+
+    $this->assertTrue($exception_caught, 'Calling getInputType() when no input type has been set should have thrown an exception but did not.');
+    $this->assertStringContainsString(
+      $expected_message,
+      $exception_message,
+      'The exception thrown does not have the message we expected when trying to get input type but none have been set yet.'
+    );
+
+    // Try to set invalid input types.
+    // Exception message should trigger.
+    foreach ($this->invalid_input_types as $input_type) {
+      $expected_message = "Input type $input_type is not supported by this validator.";
+
+      $exception_caught = FALSE;
+      $exception_message = 'NONE';
+      try {
+        $this->instance->setInputType($input_type);
+      }
+      catch (\Exception $e) {
+        $exception_caught = TRUE;
+        $exception_message = $e->getMessage();
+      }
+
+      $this->assertTrue($exception_caught, 'Calling setInputType() with an invalid input type should have thrown an exception but did not.');
+      $this->assertStringContainsString(
+        $expected_message,
+        $exception_message,
+        'The exception thrown does not have the message we expected when trying to set input type with an array that has an invalid value.'
+      );
+    }
+
+    // Set valid input types and then check that they've been set.
+    foreach ($this->valid_input_types as $input_type) {
+      $exception_caught = FALSE;
+      $exception_message = 'NONE';
+      try {
+        $this->instance->setInputType($input_type);
+      }
+      catch (\Exception $e) {
+        $exception_caught = TRUE;
+        $exception_message = $e->getMessage();
+      }
+      $this->assertFalse(
+        $exception_caught,
+        "Calling setInputType() with a valid input type should not have thrown an exception but it threw '$exception_message'"
+      );
+
+      // Check that we can get the input type we just set.
+      $grabbed_input_type = $this->instance->getInputType();
+      $this->assertEquals(
+        $input_type,
+        $grabbed_input_type,
+        'Could not grab the set input type using getInputType() despite having called setInputType() on it.'
+      );
+    }
+  }
+
+}
