cb70643e5cdc11aaed7633929f6f42f636fd2f94
Vidusha Wilpita <132868285+vidu-wil@users.noreply.github.com>
2026-06-25 16:17:41 -0600

    Applied the suggested change.
    Co-authored-by: Carolyn T Caron <carolyn.caron@usask.ca>

--- src/Plugin/Validators/ValidOrganism.php
+++ src/Plugin/Validators/ValidOrganism.php
@@ -358,8 +358,8 @@ class ValidOrganism extends TripalCultivateValidatorBase implements ContainerFac
       else {
         $org_buddy_records = $this->organism_buddy->getOrganismFromScientificName($cell);
         $organism_id = 0;
-        if (isset($organism_obj[0])) {
-          $organism_id = $organism_obj[0]->getValue('organism.organism_id');
+        if (isset($org_buddy_records[0])) {
+          $organism_id = $org_buddy_records[0]->getValue('organism.organism_id');
         }
 
         // Check for missing organism.
